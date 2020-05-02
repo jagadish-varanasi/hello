e47d059427c8c736f3025f4d99ff5f98f781b4a0
Jaga <jagadishvaranasi123@gmail.com>
2020-05-02 22:29:58 +0530

    adding approval list

---
+++ src/ApprovalCard.js
@@ -0,0 +1,25 @@
+import React from 'react'
+//import ReactDOm from 'react-dom'
+//u are getting propos from index page when entire component is sent as property
+//so use .children to extract that value of preoperty
+
+const ApprovalCard=(props)=>{
+    console.log(props)
+  return(
+    <div className="ui card">
+      <div className="content">
+          {props.children}
+      </div>
+          <div className="extra content">
+              <div className="ui two buttons">
+                  <div className="ui basic green button">Approve</div>
+                  <div className="ui basic red button">Reject</div>
+              </div>
+        </div>
+    </div>
+
+       
+  );
+};
+
+export default ApprovalCard;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import CommentDetail from './CommentDetail'
 import faker from 'faker'
+import ApproveCard from './ApprovalCard'
 // identify the portion that will be duplicated and make it a sepate component
 //we cannot use { } while showing components.. always treat components as tags
 // Here App is parent component and CommentDetail is child component...props sysytem is a sysytem in react for 
@@ -11,7 +12,9 @@ import faker from 'faker'
 const App=()=>{
   return (
    <div className="ui container comments">
+    <ApproveCard>
    <CommentDetail author="sam" timeAgo="8PM" content="Nice Blog" imagesrc={faker.image.avatar()} />
+   </ApproveCard>
    <CommentDetail author="Bob" timeAgo="9PM" content="It was good" imagesrc={faker.image.avatar()} />
    <CommentDetail author="Alice" timeAgo="6AM" content="It is awesome" imagesrc={faker.image.avatar()} />
    </div>
