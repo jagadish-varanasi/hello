5f446d73f5916b805b0a8e2eb2a0718779323753
Jaga <jagadishvaranasi123@gmail.com>
2020-05-02 21:15:28 +0530

    taking duplicate content as making a component

--- src/index.js
+++ src/index.js
@@ -1,58 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import faker from 'faker'
-
+import CommentDetail from './CommentDetail'
+// identify the portion that will be duplicated and make it a sepate component
+//we cannot use { } while showing components.. always treat components as tags
 const App=()=>{
   return (
    <div className="ui container comments">
-      <div className="comment">
-         <a href="/" className="avatar">
-         <img alt="avatar" src={faker.image.avatar()}/>
-         </a>
-         <div className="content">
-           <a href="/" className="author">
-             Sam
-           </a>
-           <div className="metadata">
-           <span className="date">Today at 6:00PM</span>
-           </div>
-           <div className="text">Nice blog Post</div>
-
-         </div>
-      </div>
-
-      <div className="comment">
-         <a href="/" className="avatar">
-         <img alt="avatar" src={faker.image.avatar()}/>
-         </a>
-         <div className="content">
-           <a href="/" className="author">
-             Sam
-           </a>
-           <div className="metadata">
-           <span className="date">Today at 6:00PM</span>
-           </div>
-           <div className="text">Nice blog Post</div>
-
-         </div>
-      </div>
-
-      <div className="comment">
-         <a href="/" className="avatar">
-         <img alt="avatar" src={faker.image.avatar()}/>
-         </a>
-         <div className="content">
-           <a href="/" className="author">
-             Sam
-           </a>
-           <div className="metadata">
-           <span className="date">Today at 6:00PM</span>
-           </div>
-           <div className="text">Nice blog Post</div>
-
-         </div>
-      </div>
-</div>
+   <CommentDetail/>
+   </div>
 
 
   );
