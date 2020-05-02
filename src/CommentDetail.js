c83d69270baa1779ba95b2203c77da7449770d60
Jaga <jagadishvaranasi123@gmail.com>
2020-05-02 21:31:43 +0530

    adding parent and child comp and props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import faker from 'faker'
-
-const CommentDetail=()=>{
+//Here we should consume the properties provided by parent component...the property called author
+//the aurgument to the component is props...when u console log it u'll get key value pair like {author:'Bob'}....
+// to access this value use {props.author} in child component
+const CommentDetail=(props)=>{
+    console.log(props)
     return(
         <div className="comment">
         <a href="/" className="avatar">
@@ -9,7 +12,7 @@ const CommentDetail=()=>{
         </a>
         <div className="content">
           <a href="/" className="author">
-            Sam
+            {props.author}
           </a>
           <div className="metadata">
           <span className="date">Today at 6:00PM</span>
--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom'
 import CommentDetail from './CommentDetail'
 // identify the portion that will be duplicated and make it a sepate component
 //we cannot use { } while showing components.. always treat components as tags
+// Here App is parent component and CommentDetail is child component...props sysytem is a sysytem in react for 
+//passing data from parent to child component..goal is to customize and configure child component...communicate data from p to c...props means properties...
+//parent provides info and child consumes this info to display that particular propertiy value
+
 const App=()=>{
   return (
    <div className="ui container comments">
-   <CommentDetail/>
+   <CommentDetail author="sam" />
+   <CommentDetail author="Bob"/>
+   <CommentDetail author="Alice"/>
    </div>
 
 
